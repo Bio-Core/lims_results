78b24d161c57ae286b6226ba8b8d8f09e7a0f8c0
TomWuTheCoop <35300794+TomWuTheCoop@users.noreply.github.com>
2018-02-14 11:36:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,19 +9,35 @@ container_app_host = '0.0.0.0'
 
 container_results_port = 8003
 
-@app.route('/')
-def index():
+@app.route('/query')
+def queryBuilder():
     mysql_connector.connect()
     tables = mysql_connector.table_names
-    return render_template('test.html', list1=tables)
-
+    return render_template('queryBuilder.html', list1=tables)
+'''
 @app.route('/login')
 def login():
     return render_template('login.html')
-
+'''
 @app.route('/edit')
 def edit():
     return render_template('edit.html')
+
+@app.route('/patients')
+def patients():
+    mysql_connector.preview('patients')
+    preview = mysql_connector.previews
+    return render_template('patients.html', list1=preview)
+
+@app.route('/samples')
+def samples():
+    mysql_connector.preview('samples')
+    preview = mysql_connector.previews
+    return render_template('samples.html', list1=preview)
+
+@app.route('/query-profile')
+def profile():
+    return render_template('saveQuery.php')
 
 if __name__ == '__main__':
     app.run(debug=True, host=container_app_host, port=container_results_port)
