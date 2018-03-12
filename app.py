07732a1554e186e5691e615b6755f2a07184bc68
TomWuTheCoop <35300794+TomWuTheCoop@users.noreply.github.com>
2018-03-12 12:17:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-from database import mysql_connector
 
 app = Flask(__name__)
 
@@ -11,9 +10,6 @@ container_results_port = 8003
 
 @app.route('/query')
 def queryBuilder():
-#    mysql_connector.connect()
-#    tables = mysql_connector.table_names
-#    return render_template('queryBuilder.html', list1=tables)
     return render_template('queryBuilder.html')
 '''
 @app.route('/login')
@@ -23,25 +19,34 @@ def login():
 @app.route('/edit')
 def edit():
     return render_template('edit.html')
-'''
-@app.route('/patients')
-def patients():
-#    mysql_connector.preview('patients')
-#    preview = mysql_connector.previews
-#    return render_template('patients.html', list1=preview)
+
+@app.route('/patients/<id>')
+def patients(id):
     return render_template('patients.html')
 
-@app.route('/samples')
-def samples():
-#    mysql_connector.preview('samples')
-#    preview = mysql_connector.previews
-#    return render_template('samples.html', list1=preview)
+@app.route('/samples/<id>')
+def samples(id):
     return render_template('samples.html')
+
+@app.route('/experiments/<id>')
+def tests(id):
+    return render_template('tests.html')
+
+@app.route('/results/<id>')
+def results(id):
+    return render_template('results.html')
+
+@app.route('/resultdetails/<id>')
+def resultdetails(id):
+    return render_template('resultdetails.html')
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
 
 @app.route('/query-profile')
 def profile():
     return render_template('saveQuery.php')
-'''
+
 if __name__ == '__main__':
     app.run(debug=True, host=container_app_host, port=container_results_port)
-    # host=container_app_host
